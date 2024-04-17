b87620148c67e5b15618d48671d29bbbf369c4f9
Verified ✔️ <ryesan58@gmail.com>
2024-04-17 19:59:58 -0400

    setup the path for student/veiwer

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Jobs from './components/jobs';
+import {BrowserRouter, Routes, Route, link, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <h1>
-        HELLO
-        <Jobs/>
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/student/viewer' element={<Jobs/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
