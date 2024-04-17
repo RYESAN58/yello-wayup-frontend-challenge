12f3b5bb11547f139ff10b077f5a647e69e5d7a8
Verified ✔️ <ryesan58@gmail.com>
2024-04-17 19:47:30 -0400

    got the api running

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Jobs from './components/jobs';
 
 function App() {
   return (
     <div>
       <h1>
         HELLO
+        <Jobs/>
       </h1>
     </div>
   );
--- app/src/components/jobs.jsx
+++ app/src/components/jobs.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import axios from "axios";
 
 
 
 const Jobs = () => {
-  
+  const [Jobs, setJobs] = useState([]);
+  useEffect(() => {
+    axios.get('https://62bc8d086b1401736cfcd8fb.mockapi.io/jobs')
+    .then((response)=> {
+      const result = response.data;
+      console.log(result)
+      setJobs(result)
+      console.log(Jobs)
+    }).catch((err)=> {
+      console.log(err.response)
+    })
+    return () => {
+      
+    }
+  }, [])
+  return(
+    <div>
+      {
+        Jobs.map((job,key)=>{
+          return(
+            <div key={key}>
+              {job.company}
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 
