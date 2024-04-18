ce6a910d5bf99ec067436ed34192aa79a6cb3540
Verified ✔️ <ryesan58@gmail.com>
2024-04-17 22:32:23 -0400

    added a header

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { react } from "react";
+// import '../../public/'
 
 const Header = () => {
   return(
     <div>
-      HEADER
+      <img src="https://hrtechfeed.com/wp-content/uploads/2021/07/wayup-1024x576.png" alt="logo of the company Yello mxed with Wayup's logo as well" style={{width:"15%",height:"5%"}}/>
     </div>
   )
 }
