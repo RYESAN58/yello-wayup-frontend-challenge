e9b4e6f095193c3b5bca4906cb54a12285d91ed9
Verified ✔️ <ryesan58@gmail.com>
2024-04-17 20:30:53 -0400

    added the data into a table

--- app/src/components/jobs.jsx
+++ app/src/components/jobs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-// import { Card, Typography } from "@material-tailwind/react";
+import { Card, Typography } from "@material-tailwind/react";
 
 const Jobs = () => {
   const [Jobs, setJobs] = useState([]);
@@ -18,18 +18,78 @@ const Jobs = () => {
       
     }
   }, [])
+  const TABLE_HEAD = ["Title", "Company", "Description", "Location", ""];
+
   return(
-    <div>
-      {
-        Jobs.map((job,key)=>{
-          return(
-            <div key={key}>
-              {job.company}
-            </div>
-          )
-        })
-      }
-    </div>
+    <Card>
+      <table>
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head, key) => (
+              <th
+                key={key}
+                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+              >
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {Jobs.map(({ title, company, description,tags,location }, index) => {
+            const isLast = index === Jobs.length - 1;
+            const classes = isLast ? "p-4" : "p-4 border-b border-blue";
+            return (
+              <tr key={index}>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue"
+                    className="font-normal"
+                  >
+                    {title}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue"
+                    className="font-normal"
+                  >
+                    {company}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue"
+                    className="font-normal"
+                  >
+                    {description}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    as="a"
+                    variant="small"
+                    color="blue"
+                    className="font-medium"
+                  >
+                    {location}
+                  </Typography>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Card>
   )
 }
 
