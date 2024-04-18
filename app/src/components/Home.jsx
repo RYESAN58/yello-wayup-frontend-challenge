0b570a206b7321d8aa54a8eb096131d87e719d55
Verified ✔️ <ryesan58@gmail.com>
2024-04-17 23:27:42 -0400

    added home page for ui and userabilty

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-
-
+import { useNavigate } from "react-router-dom";
+import '../App.css';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    navigate(path)
+  }
   return(
-    <div>
-      HOME
+    <div className="home-page">
+      <div className="card">
+      <h2>Welcome!</h2>
+      <p>Get started by clicking the buttons below.</p>
+        <button onClick={() => handleClick('/student/viewer')}>Veiw Jobs</button>
+        <a href="https://yello.co/">
+        <button>Visit Us!</button>
+        </a>
+      </div>
     </div>
   )
 }
